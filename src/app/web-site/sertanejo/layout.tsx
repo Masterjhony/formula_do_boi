4ca302e87425162304d71e6a6344d94f4961f5f3
Masterjhony <joaoeduardo.lp1@gmail.com>
2026-03-24 15:39:22 -0300

    E la vamos

--- src/app/web-site/sertanejo/layout.tsx
+++ src/app/web-site/sertanejo/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 
-const OG_IMAGE = 'https://res.cloudinary.com/dkh2nsugb/image/upload/w_1200,h_630,c_fit,b_rgb:141413/v1774271580/sertanejo-04_a2avtq.jpg';
+const OG_IMAGE = 'https://res.cloudinary.com/dkh2nsugb/video/upload/w_1200,h_630,c_fit,b_rgb:141413,so_2/v1774293296/intro_sertantejo_t6bxhk.jpg';
 
 export const metadata: Metadata = {
   title: 'Sertanejo Terra Brava | Fórmula do Boi',
